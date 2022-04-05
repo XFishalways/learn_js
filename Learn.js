d3086c126c69dd2719596dc986ba33718135af4e
XFishalways <18901030225@163.com>
2022-04-05 15:07:01 +0800

    1 file modified

--- Learn.js
+++ Learn.js
@@ -1,9 +1,11 @@
-let value = prompt('Type a number', 0);
+let admin;
 
-if (value > 0) {
-  alert( 1 );
-} else if (value < 0) {
-  alert( -1 );
-} else {
-  alert( 0 );
+prompt("Enter the password", admin);
+
+if(admin==TheMaster){
+  alert("Welcome!");
+} else if(admin){
+  alert("Wrong password");
+} else{
+  alert("Canceled");
 }

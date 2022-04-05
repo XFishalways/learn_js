5b94eec15300b1f8e344f1d74abc36ea61dd4728
XFishalways <18901030225@163.com>
2022-04-05 20:35:18 +0800

    1 file modified

--- Learn.js
+++ Learn.js
@@ -1,1 +1,9 @@
-alert("welcome");
+let admin=prompt("Enter the password", "");
+
+if(admin=="TheMaster"){
+  alert("Welcome!");
+} else if(admin){
+  alert("Wrong password");
+} else{
+  alert("Canceled");
+}

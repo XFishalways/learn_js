6472ae78c801e9dd3ac2d1e81cc943dbb57f58f1
XFishalways <18901030225@163.com>
2022-04-08 21:52:25 +0800

    1 file modified

--- Learn.js
+++ Learn.js
@@ -1,17 +1,14 @@
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep() {
-    alert( this.step );
-    return this;
-  }
-};
+function Accumulator (value) {
+  this.value = +value;
+  this.read = function () {
+    this.addValue = +prompt ("type a number", "");
+    this.value += this.addValue;
+  };
+}
 
-ladder.up().up().down().showStep().down().showStep();
+let accumulator = new Accumulator(1); // 初始值 1
+
+accumulator.read(); // 添加用户输入的 value
+accumulator.read(); // 添加用户输入的 value
+
+alert(accumulator.value); // 显示这些值的总和

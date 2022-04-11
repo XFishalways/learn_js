39e70364b02638986cbea82c561b809d5d2dad03
XFishalways <18901030225@163.com>
2022-04-11 16:18:21 +0800

    1 file modified

--- Learn.js
+++ Learn.js
@@ -1,5 +1,11 @@
-function extractCurrencyValue (str) {
-  return str.slice(1);
-}
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let partialSum = 0;
 
-alert( extractCurrencyValue('$120') );
+  for (let item of arr) {
+    partialSum += item;
+    maxSum = Math.max(maxSum, partialSum);
+    if (partialSum < 0) partialSum = 0;
+  }
+  return maxSum;
+}

1a0c3af2653255d10b27dd5b762fac08980c12c3
XFishalways <18901030225@163.com>
2022-04-13 21:49:13 +0800

    learning updated

--- Learn.js
+++ Learn.js
@@ -1,3 +1,17 @@
-let arr = ["Bilbo", "Gandalf", "Nazgul"]
+function filterRange (arr, a, b) {
+    let filteredarr = [];
+    for(let num in arr) {
+        if (+num >= a && +num <= b) {
+            filteredarr.push(num);
+        }
+    }
+    return filteredarr;
+}
 
-arr.forEach(alert);
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1（匹配值）
+
+alert( arr ); // 5,3,8,1（未修改）

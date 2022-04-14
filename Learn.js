0baead3fa5aae0607a014efbc02bd5d18f32c135
XFishalways <18901030225@163.com>
2022-04-14 12:03:22 +0800

    learning updated

--- Learn.js
+++ Learn.js
@@ -8,6 +8,9 @@ function Calculator() {
     }
     this.calculate = function (str) {
         let realstr = str.split(" ");
+        if(!this.method[realstr[1] || isNaN(a) || isNaN(b)]) {
+            return "invalid calculate";
+        }
         return this.method[realstr[1]](realstr[0], realstr[2]);
     }
 }
@@ -23,3 +26,5 @@ powerCalc.addMethod("**", (a, b) => a ** b);
 
 let result = powerCalc.calculate("2 ** 3");
 alert( result ); // 8
+
+alert((powerCalc.calculate("3 ^ 0")));

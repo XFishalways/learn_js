c616bf9cf914c7f5c34ac11b03a7ece2291b0808
XFishalways <18901030225@163.com>
2022-04-21 20:24:42 +0800

    1 file modified

--- Learn.js
+++ Learn.js
@@ -1,25 +1,7 @@
-let list = {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null
-        }
-      }
-    }
-  };
-  
-  function printList(list) {
-  
-    if (list.next) {
-      printList(list.next); // 链表中其余部分同理
-    }
+let obj = { a: 1, b: 2, c: 3 };
 
-    alert(list.value); // 输出当前元素
-  
-  }
-  
-  printList(list);
+let objCopy = { ...obj }; // 将对象 spread 到参数列表中
+                          // 然后将结果返回到一个新对象
+
+// 两个对象中的内容相同吗？
+alert(JSON.stringify(obj) === JSON.stringify(objCopy)); // true

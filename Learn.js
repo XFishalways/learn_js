5783c281443256dcd1f260fc18ca977413805685
XFishalways <18901030225@163.com>
2022-04-21 21:53:05 +0800

    1 file modified

--- Learn.js
+++ Learn.js
@@ -1,7 +1,9 @@
-let obj = { a: "r", b: 2, c: 3 };
+let name = "John";
 
-let objCopy = { ...obj }; // 将对象 spread 到参数列表中
-                          // 然后将结果返回到一个新对象
+function sayHi() {
+  alert("Hi, " + name);
+}
 
-// 两个对象中的内容相同吗？
-alert(JSON.stringify(obj) === JSON.stringify(objCopy)); // true
+name = "Pete";
+
+alert(name);

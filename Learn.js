a82aef53f95220a75487a18f6a81997bcf2146a2
XFishalways <18901030225@163.com>
2022-05-03 17:32:14 +0800

    learning updated

--- Learn.js
+++ Learn.js
@@ -1,21 +1,10 @@
-function f(x) {
-  alert(x);
-}
+let reverse = function(x) {
+  let arr = x.split("");
+  let rev = arr.reverse();
+  let join = rev.join("");
+  return join;
+};
 
-// create wrappers
-let f1000 = delay(f, 1000);
-let f1500 = delay(f, 1500);
+let str = "123";
 
-f1000("test"); // 在 1000ms 后显示 "test"
-f1500("test"); // 在 1500ms 后显示 "test"
-
-function delay(f, ms) {
-
-  return function(...args) {
-    let savedThis = this; // 将 this 存储到中间变量
-    setTimeout(function() {
-      f.apply(savedThis, args); // 在这儿使用它
-    }, ms);
-  };
-
-}
+alert(reverse(str));

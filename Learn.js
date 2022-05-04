518b3b5a15415d1fcbce38de98950172583fe4db
XFishalways <18901030225@163.com>
2022-05-04 21:31:22 +0800

    1 file modified

--- Learn.js
+++ Learn.js
@@ -1,10 +1,28 @@
-let reverse = function(x) {
-  let arr = x.split("");
-  let rev = arr.reverse();
-  let join = rev.join("");
-  return join;
-};
+function throttle(func, ms) {
 
-let str = "123";
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
 
-alert(reverse(str));
+  function wrapper() {
+
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+    isThrottled = true;
+
+    func.apply(this, arguments); // (1)
+
+    setTimeout(function() {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
